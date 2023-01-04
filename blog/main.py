478194bba51300d43c78919fa7087ddf54c8a0bb
RRobby <rodhiyya.robby@gmail.com>
2023-01-04 07:23:19 +0700

    store blog to database

--- blog/main.py
+++ blog/main.py
@@ -7,3 +7,20 @@ app = FastAPI()
 
 
 models.Base.metadata.create_all(bind=engine)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.post("/blog")
+def create(request: schemas.Blog, db: Session = Depends(get_db)):
+    new_blog = models.Blog(title=request.title, body=request.body)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
