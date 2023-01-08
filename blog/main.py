337af77573c2561ee8dba8e651856fc07ae542e2
RRobby <rodhiyya.robby@gmail.com>
2023-01-08 07:09:27 +0700

    update blog

--- blog/main.py
+++ blog/main.py
@@ -34,13 +34,28 @@ def all(db: Session = Depends(get_db)):
 
 @app.delete("/blog/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def destroy(id, response: Response, db: Session = Depends(get_db)):
-    blog = (
-        db.query(models.Blog)
-        .filter(models.Blog.id == id)
-        .delete(synchronize_session=False)
-    )
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Blog with the id {id} is not found",
+        )
+    blog.delete(synchronize_session=False)
     db.commit()
     return "done"
+
+
+@app.put("/blog/{id}", status_code=status.HTTP_202_ACCEPTED)
+def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Blog with the id {id} is not found",
+        )
+    blog.update(request.dict())
+    db.commit()
+    return "updated"
 
 
 @app.get("/blog/{id}", status_code=200)
