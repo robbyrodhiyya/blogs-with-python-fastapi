acdca1642b7c7b7a2c6c92ab10d27fbece5b2303
RRobby <rodhiyya.robby@gmail.com>
2023-01-08 11:09:02 +0700

    add relation

--- blog/schemas.py
+++ blog/schemas.py
@@ -1,17 +1,5 @@
+from typing import List
 from pydantic import BaseModel
-
-
-class Blog(BaseModel):
-    title: str
-    body: str
-
-
-class ShowBlog(BaseModel):
-    title: str
-    body: str
-
-    class Config:
-        orm_mode = True
 
 
 class User(BaseModel):
@@ -20,9 +8,29 @@ class User(BaseModel):
     password: str
 
 
+class BlogBase(BaseModel):
+    title: str
+    body: str
+
+
+class Blog(BlogBase):
+    class Config:
+        orm_mode = True
+
+
 class ShowUser(BaseModel):
     name: str
     email: str
+    blogs: List[Blog] = []
+
+    class Config:
+        orm_mode = True
+
+
+class ShowBlog(BaseModel):
+    title: str
+    body: str
+    creator: ShowUser
 
     class Config:
         orm_mode = True
