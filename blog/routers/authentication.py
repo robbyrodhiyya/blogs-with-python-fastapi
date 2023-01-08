e20dd821a50a643603204af64bbe8447edcd4070
RRobby <rodhiyya.robby@gmail.com>
2023-01-08 16:04:11 +0700

    route behind auth

--- blog/routers/authentication.py
+++ blog/routers/authentication.py
@@ -1,4 +1,5 @@
 from datetime import timedelta
+from fastapi.security import OAuth2PasswordRequestForm
 from fastapi import APIRouter, Depends, HTTPException, status
 from .. import schemas, database, models, token
 from sqlalchemy.orm import Session
@@ -9,7 +10,10 @@ router = APIRouter(tags=["Authentication"])
 
 
 @router.post("/login")
-def login(request: schemas.Login, db: Session = Depends(database.get_db)):
+def login(
+    request: OAuth2PasswordRequestForm = Depends(),
+    db: Session = Depends(database.get_db),
+):
     user = db.query(models.User).filter(models.User.email == request.username).first()
     if not user:
         raise HTTPException(
@@ -22,5 +26,3 @@ def login(request: schemas.Login, db: Session = Depends(database.get_db)):
 
     access_token = token.create_access_token(data={"sub": user.email})
     return {"access_token": access_token, "token_type": "bearer"}
-
-    return user
